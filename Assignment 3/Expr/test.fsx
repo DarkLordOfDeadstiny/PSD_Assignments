784d763e702242b93c9a74ba9bd164fe1531ef93
Patrick <patr7500@gmail.com>
2022-09-15 17:00:16 +0200

    add test script for assignment 3

---
+++ Assignment 3/Expr/test.fsx
@@ -0,0 +1,13 @@
+#r "FsLexYacc.Runtime.dll"
+
+//C:\Users\patr7\Desktop\Programmer som data\PSD_Assignments\Assignment 3\Expr\FsLexYacc.Runtime.dll
+
+#load "Absyn.fs"
+#load "ExprPar.fs"
+#load "ExprLex.fs"
+#load "Parse.fs"
+#load "Expr.fs"
+
+open Expr
+
+compString "1 + 2 * 3"
