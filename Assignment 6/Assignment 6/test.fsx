7f30bf48837dbc22ce978f7d51bdddb6d51ca695
surtshow <jonas@lindvig.net>
2022-10-13 10:37:34 +0200

    A

--- Assignment 6/Assignment 6/test.fsx
+++ Assignment 6/Assignment 6/test.fsx
@@ -1,4 +1,13 @@
 #r "nuget: FsLexYacc.Runtime"
+#load "Absyn.fs"
+#load "CPar.fs"
+#load "CLex.fs"
+#load "Parse.fs"
+#load "Interp.fs"
+#load "ParseAndRun.fs"
 
-
-
+open ParseAndRun
+   fromFile "ex1.c";;
+   run (fromFile "ex1.c") [17];;
+   run (fromFile "ex5.c");;
+   run (fromFile "ex11.c") [8];;
