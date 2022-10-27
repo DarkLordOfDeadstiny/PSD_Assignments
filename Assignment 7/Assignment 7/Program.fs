7de7de3c7c6bb359a29a2e711c498f32a3fdb061
Patrick <patr7500@gmail.com>
2022-10-27 17:29:32 +0200

    Assignment 7 init

---
+++ Assignment 7/Assignment 7/Program.fs
@@ -0,0 +1,7 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+printfn "Hello from F#"
+
+open ParseAndComp;;
+compileToFile (fromFile "ex11.c") "ex11.out" |> ignore
+compile "ex11" |> ignore
+
---
+++ Assignment 7/Assignment 7/test.fsx
@@ -0,0 +1,23 @@
+#r "nuget: FsLexYacc.Runtime"
+#load "Absyn.fs" 
+#load "CPar.fs"
+#load "CLex.fs" 
+#load "Parse.fs"
+#load "Machine.fs" 
+#load "Comp.fs"
+#load "ParseAndComp.fs"
+
+
+//fsharpi -r ~/fsharp/FsLexYacc.Runtime.dll Absyn.fs CPar.fs CLex.fs Parse.fs Machine.fs Comp.fs ParseAndComp.fs   
+
+open ParseAndComp;;
+compileToFile (fromFile "examples/ex11.c") "examples/ex11.out";;
+compile "examples/ex11";;
+
+compileToFile (fromFile "examples/ex3.c") "examples/ex3.out";;
+compile "examples/ex3";;
+
+compileToFile (fromFile "examples/ex5.c") "examples/ex5.out";;
+compile "examples/ex5";;
+
+
